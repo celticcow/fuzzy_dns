ede64899493efc7b3a67ecc5bb5c3173fb7ebcad
Celticcow <gregory.dunlap@gmail.com>
2020-10-22 09:11:56 -0500

    init files

---
+++ conn.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+class defied to keep up with a connecton
+
+source IP 
+dest IP
+Port (53 for our case here)
+
+hit-count
+"""
---
+++ fuzzy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import csv
+import ipaddress
+
+"""
+trying to find bad dns calls
+fuzzy logic around ip schema
+"""
+
+
+def main():
+    print("start of fuzzy dns")
+
+    inputfile = "dns-out.csv"
+
+if __name__ == "__main__":
+    main()
