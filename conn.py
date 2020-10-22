622e8ea33c2585c891e5c958eeab83313669c291
Celticcow <gregory.dunlap@gmail.com>
2020-10-22 13:47:27 -0500

    log output

--- conn.py
+++ conn.py
@@ -9,3 +9,40 @@ Port (53 for our case here)
 
 hit-count
 """
+
+class conn(object):
+    ## constructor
+    def __init__(self, source="0.0.0.0", dest="0.0.0.0", port="0"):
+        self.source = source
+        self.dest = dest
+        self.port = port
+        self.count = 0
+
+    #modifiers
+    def set_source(self, source):
+        self.source = source
+    
+    def set_dest(self, dest):
+        self.dest = dest
+    
+    def set_port(self, port):
+        self.port = port
+    
+    def increment_count(self):
+        self.count = self.count + 1
+    
+    #accessors
+    def get_source(self):
+        return(self.source)
+    
+    def get_dest(self):
+        return(self.dest)
+    
+    def get_port(self):
+        return(self.port)
+
+    def get_count(self):
+        return(self.count)
+
+    def print_conn(self):
+        print(self.source + " -> " + self.dest + " : " + self.port + " occured " + self.count + " times")
--- fuzzy.py
+++ fuzzy.py
@@ -3,16 +3,78 @@
 import csv
 import ipaddress
 
+from conn import conn
+
 """
 trying to find bad dns calls
 fuzzy logic around ip schema
 """
 
+def read_file(file_name):
+    print("reading file")
+
+    debug = 1
+
+    with open(file_name) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            src = row[0]
+            dst = row[1]
+            port = row[2]
+
+            if(fuzzy_match(dst) == False):
+                print(src + " -> " + dst + " : " + port)
+                #fuzzy_match(dst)
+            #much more to come here
+
+#end_of_read_file
+
+def fuzzy_match(ip_addr):
+    """
+    some statics
+    199.82.243.70
+    146.18.173.70
+    """
+    if(ip_addr == "199.82.243.70" or ip_addr == "146.18.173.70"):
+        #print(" outdated_dns server ")
+        return(False)
+    
+    if("192.82.243.70" in ip_addr):
+        #print(" misconfig dns ")
+        return(False)
+
+    if("199.112.46" in ip_addr):
+        #print(" anycast set wrong ")
+        return(False)
+    
+    if("199.81.46" in ip_addr):
+        #print(" anycast set wrong ")
+        return(False)
+    
+    if("198.112.46" in ip_addr):
+        #print(" anycast set wrong ")
+        return(False)
+    
+    if("192.112.45.53" in ip_addr):
+        #print(" anycast set wrong ")
+        return(False)
+
+    if("192.116.46.53" in ip_addr):
+        #print(" anycast set wrong ")
+        return(False)
+    
+    if(ip_addr == "8.8.4.4" or ip_addr == "8.8.8.8"):
+        #print(" the googles ")
+        return(False)
+    
+    return(True)
 
 def main():
     print("start of fuzzy dns")
 
     inputfile = "dns-out.csv"
+
+    read_file(inputfile)
 
 if __name__ == "__main__":
     main()
